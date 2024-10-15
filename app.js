905773f1e3ddb6e2dc2a84e5ecfd49c77135cfb9
João Igor Andrade <110249417+isandradeigor@users.noreply.github.com>
2024-10-15 19:27:58 -0300

    commit1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, function (erro) {
+    if (erro) {
+        console.log("Erro ao iniciar");
+    } else {
+        console.log("Servidor iniciado");
+        console.log("IP do servidor: http://localhost:3000");
+    }
+})
+
+app.get("/", function (req, res) {
+    res.send("Bem-vindo a primeira pagina OLA!");
+})
+
+app.get("/produtos", function (req, res) {
+    res.send("Comprar item OLA!");
+})
+
+app.get("/perfil", function (req, res) {
+    res.send("Pagina de perfil OLA.");
+})
+
+app.get("/home", function (req, res) {
+    res.send("Home page, bem-vindo OLA!");
+})
+
+app.get("/OLA", function (req, res) {
+    res.send("Home page, bem-vindo OLA!");
+})
